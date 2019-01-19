b06e2f69437c6441e410c30a7e499d09ada5a125
tommyshem <1952659+tommyshem@users.noreply.github.com>
2019-01-19 22:43:05 +0000

    moved basic functions

--- rust.lua
+++ rust.lua
@@ -1,16 +1,22 @@
 VERSION = "0.1.0"
-
-if GetOption("goimports") == nil then
-    AddOption("goimports", false)
+-- Micro Editor options for this plugin
+-- cargo formating project
+if GetOption("cargofmt") == nil then
+    AddOption("cargofmt", false)
 end
-if GetOption("gofmt") == nil then
-    AddOption("gofmt", true)
+-- rustfmt file only
+if GetOption("rustfmt") == nil then
+    AddOption("rustfmt", true)
 end
-
-MakeCommand("goimports", "go.goimports", 0)
-MakeCommand("gofmt", "go.gofmt", 0)
-MakeCommand("gorename", "go.gorename", 0)
-
+-- rustfmt backup file
+if GetOption("rustbackup") == nil then
+    AddOption("rustbackup", false)
+end
+-- clippy linter 
+if GetOption("rustclippy") == nil then
+    AddOption("rustclippy", false)
+end
+-- Micro editor Callback functions below
 function onViewOpen(view)
     if view.Buf:FileType() == "go" then
         SetLocalOption("tabstospaces", "off", view)
@@ -18,73 +24,50 @@ function onViewOpen(view)
 end
 
 function onSave(view)
-    if CurView().Buf:FileType() == "go" then
-        if GetOption("goimports") then
-            goimports()
-        elseif GetOption("gofmt") then
-            gofmt()
+    if CurView().Buf:FileType() == "rust" then
+        if GetOption("rustfmt") then
+            rustfmt()
+        elseif GetOption("cargofmt") then
+            cargofmt()
         end
+    if GetOption("rustclippy") then
+        rustclippy()
+    end
     end
 end
 
-function gofmt()
+-- Functions below for this plugin
+function rustfmt()
     CurView():Save(false)
-    local handle = io.popen("gofmt -w " .. CurView().Buf.Path)
+    local handle = io.popen("rustfmt " .. CurView().Buf.Path)
     local result = handle:read("*a")
     handle:close()
-
     CurView():ReOpen()
 end
 
-function gorename()
-    local res, canceled = messenger:Prompt("Rename to:", "", 0)
-    if not canceled then
-        gorenameCmd(res)
-        CurView():Save(false)
-    end
-end
-
-function gorenameCmd(res)
+function cargofmt()
     CurView():Save(false)
-    local v = CurView()
-    local c = v.Cursor
-    local buf = v.Buf
-    local loc = Loc(c.X, c.Y)
-    local offset = ByteOffset(loc, buf)
-    if #res > 0 then
-        local cmd = "gorename --offset " .. CurView().Buf.Path .. ":#" .. tostring(offset) .. " --to " .. res
-        JobStart(cmd, "", "go.renameStderr", "go.renameExit")
-        messenger:Message("Renaming...")
-    end
+    local handle = io.popen("cargo-fmt ")
+    local result = handle:read("*a") -- , ":")
+    handle:close()
+    CurView():ReOpen()
 end
 
-function renameStderr(err)
+function rustclippy()
+    CurView():Save(false)
+    local handle = io.popen("cargo clippy " .. CurView().Buf.Path)
+    local result = handle:read("*a") -- , ":")
+    handle:close()
+    CurView():ReOpen()
+end
+
+function displayerrormessage(err)
     messenger:Error(err)
 end
 
-function renameExit()
-    CurView():ReOpen()
-    messenger:Message("Done")
-end
-
-function goimports()
-    CurView():Save(false)
-    local handle = io.popen("goimports -w " .. CurView().Buf.Path)
-    local result = split(handle:read("*a"), ":")
-    handle:close()
-
-    CurView():ReOpen()
-end
-
-function split(str, sep)
-    local result = {}
-    local regex = ("([^%s]+)"):format(sep)
-    for each in str:gmatch(regex) do
-        table.insert(result, each)
-    end
-    return result
-end
-
-AddRuntimeFile("go", "help", "help/go-plugin.md")
-BindKey("F6", "go.gorename")
-MakeCommand("gorename", "go.gorenameCmd", 0)
+-- Micro Editor help file for this plugin
+AddRuntimeFile(rust, "help", "help/rust-plugin.md")
+-- Micro Editor binkeys for this plugin
+--indKey("F6", "rust.rustfmt")
+-- Micro Editor commands added from this plugin
+MakeCommand("rustfmt", "rust.rustfmt", 0)
