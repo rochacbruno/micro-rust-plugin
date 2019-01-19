012d8b4894501a79ca5dbd627235c82362516e0f
tommyshem <1952659+tommyshem@users.noreply.github.com>
2019-01-19 23:27:44 +0000

    renamed options

--- help/rust-plugin.md
+++ help/rust-plugin.md
@@ -1,8 +1,8 @@
-# Go Plugin
+# Rust Plugin
 
 The rust plugin provides some extra niceties for using micro with
 the Rust programming language. The main thing this plugin does is
-run `rustfmt` or `cargo fmt` for you automatically. 
+run `rustfmt` or `cargo-fmt` for you automatically. 
 
 Runs linter for you.
 
@@ -15,11 +15,10 @@ You can run
 To automatically run these when you save the file, use the following
 options:
 
-* `gofmt`: run gofmt on file saved. Default value: `on`
-* `goimports`: run goimports on file saved. Default value: `off`
-
-To use `gorename`, place your cursor over the variable you would like
-to rename and enter the command `> gorename newName`.
+* `rust-plugin-rustfmt`: run gofmt on file saved. Default value: `on`
+* `rust-plugin-backup`: run rustfmt on file saved and backup old file. Default value: `off`
+* `rust-plugin-cargofmt`: run cargo-fmt on project. Default value: `off`
+* `rust-plugin-rustclippy`: run cargo-clippy on project. Default value: `off`
 
 You also press F6 (the default binding) to open a prompt to rename. You
 can rebind this in your `bindings.json` file with the action `go.gorename`.
--- rust.lua
+++ rust.lua
@@ -1,20 +1,20 @@
 VERSION = "0.1.0"
 -- Micro Editor options for this plugin
 -- cargo formating project
-if GetOption("cargofmt") == nil then
-    AddOption("cargofmt", false)
+if GetOption("rust-plugin-cargofmt") == nil then
+    AddOption("rust-plugin-cargofmt", false)
 end
 -- rustfmt file only
-if GetOption("rustfmt") == nil then
-    AddOption("rustfmt", true)
+if GetOption("rust-plugin-rustfmt") == nil then
+    AddOption("rust-plugin-rustfmt", true)
 end
 -- rustfmt backup file
-if GetOption("rustbackup") == nil then
-    AddOption("rustbackup", false)
+if GetOption("rust-plugin-rustfmt-backup") == nil then
+    AddOption("rust-plugin-rustfmt-backup", false)
 end
 -- clippy linter 
-if GetOption("rustclippy") == nil then
-    AddOption("rustclippy", false)
+if GetOption("rust-plugin-rustclippy") == nil then
+    AddOption("rust-plugin-rustclippy", false)
 end
 -- Micro editor Callback functions below
 -- function onViewOpen(view)
@@ -25,21 +25,24 @@ end
 
 function onSave(view)
     if CurView().Buf:FileType() == "rust" then
-        if GetOption("rustfmt") then
-            rustfmt()
-        elseif GetOption("cargofmt") then
+        if GetOption("rust-plugin-rustfmt") then
+            rustfmt() 
+        elseif GetOption("rust-plugin-cargofmt") then
             cargofmt()
         end
-    if GetOption("rustclippy") then
+    if GetOption("rust-plugin-rustclippy") then
         rustclippy()
     end
     end
 end
 
 -- Functions below for this plugin
-function rustfmt()
+function rustfmt(backupflag)
     CurView():Save(false)
-    local handle = io.popen("rustfmt " .. CurView().Buf.Path)
+    local handle
+    if GetOption("rust-plugin-rustfmt-backup") then handle = io.popen("rustfmt --backup " .. CurView().Buf.Path)
+    else handle = io.popen("rustfmt " .. CurView().Buf.Path)
+    end
     local result = handle:read("*a")
     handle:close()
     CurView():ReOpen()
@@ -55,7 +58,7 @@ end
 
 function rustclippy()
     CurView():Save(false)
-    local handle = io.popen("cargo clippy " .. CurView().Buf.Path)
+    local handle = io.popen("cargo-clippy " .. CurView().Buf.Path)
     local result = handle:read("*a") -- , ":")
     handle:close()
     CurView():ReOpen()
@@ -70,4 +73,6 @@ AddRuntimeFile("rust", "help", "help/rust-plugin.md")
 -- Micro Editor binkeys for this plugin
 --indKey("F6", "rust.rustfmt")
 -- Micro Editor commands added from this plugin
+MakeCommand("rustfmt", "rust.rustfmt", 0)
+MakeCommand("cargofmt", "rust.cargofmt", 0)
 MakeCommand("rustfmt", "rust.rustfmt", 0)
